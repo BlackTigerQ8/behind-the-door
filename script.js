2486a3705ed461e06ea931039d8bf13057c67f64
duaij304 <69263437+duaij304@users.noreply.github.com>
2024-07-06 15:46:34 +0300

    Challenge Completed!

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Try adding /flag.txt to the URL to find the hidden flag!");
+console.log("Front-end is not the right place!");
+console.log("Try somewhere else!");
+
+let btn = document.querySelector("button");
+
+const flag = () => {
+  console.log(
+    "The flag is hiding somewhere, normally the favorite place to hide is a text file with the name flag."
+  );
+};
+
+btn.addEventListener("click", flag);
